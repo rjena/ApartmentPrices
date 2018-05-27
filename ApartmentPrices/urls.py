c4e9f878608a29da15c6f79a12e1340cbfca1b67
rjena <erzhenatasha9@yandex.ru>
2018-05-27 23:56:02 +0800

    kinda design 2

--- ApartmentPrices/urls.py
+++ ApartmentPrices/urls.py
@@ -5,6 +5,6 @@ app_name = 'ApartmentPrices'
 urlpatterns = [
     url(r'^$', views.ApartmentPrices_list, name='ApartmentPrices_list'),
     url(r'^(?P<apartment_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^apartment/new/$', views.apartment_new, name='apartment_new'),
+    url(r'^new/$', views.apartment_new, name='apartment_new'),
     url(r'api', views.apartment_api)
 ]
