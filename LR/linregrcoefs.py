058a8efe4a108440c62b78e73ab5fa90ef7a36e4
rjena <erzhenatasha9@yandex.ru>
2018-06-05 05:14:32 +0800

    test

--- LR/linregrcoefs.py
+++ LR/linregrcoefs.py
@@ -8,7 +8,6 @@ import numpy as np
 '''
 r = np.genfromtxt('/home/rjena/ApartmentPrices/LR/datawarea.csv', delimiter=';', dtype=(int, int, int, int, int, int, int, int, int, int, int, float, int, int))
 
-dataLen = len(r)    # количество квартир
 varLen = len(r[0])  # количество переменных
 
 y = []
@@ -26,17 +25,41 @@ x11 = []
 x12 = []
 x13 = []
 x = [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13]
-for i in range(0,dataLen):
-    y.append(r[i][varLen-1])
-    for j in range(0,varLen-1):
-        x[j].append(r[i][j])
 
+yt = []
+xt1 = []
+xt2 = []
+xt3 = []
+xt4 = []
+xt5 = []
+xt6 = []
+xt7 = []
+xt8 = []
+xt9 = []
+xt10 = []
+xt11 = []
+xt12 = []
+xt13 = []
+xt = [xt1,xt2,xt3,xt4,xt5,xt6,xt7,xt8,xt9,xt10,xt11,xt12,xt13]
+
+for i in range(0,len(r)):
+    if i%3!=0 and i%11!=0:
+        y.append(r[i][varLen-1])
+        for j in range(0,varLen-1):
+            x[j].append(r[i][j])
+    else:
+        yt.append(r[i][varLen-1])
+        for j in range(0,varLen-1):
+            xt[j].append(r[i][j])
+            
+print('Тестовое множество: '+'%.0f' %(len(yt)/len(r)*100) + '%')
 # Ax = B
 # A ~ (varLen,varLen) - независимые переменные
 # B ~ (1,varLen) - зависимая переменная (цена)
 # x ~ (1,varLen) - коэффициенты
-matrB = np.zeros(varLen)
-matrA = np.zeros((varLen,varLen),dtype=float)
+matrB = np.zeros(varLen) #X'y
+matrA = np.zeros((varLen,varLen),dtype=float) #X'X
+dataLen = len(y)
 
 def getMatrixes():
     matrB[0] = sum(y)
@@ -54,28 +77,45 @@ def getMatrixes():
 
 def summul(i,j):
     if i==j:
-        a = np.array(x)
-        b = np.array(x)
-        return np.sum(a * b, axis = 1)
+        return np.sum(np.array(x) * np.array(x), axis = 1)
     else:
-        a = np.array(x[i])
-        b = np.array(x[j])
-        return np.sum(a * b)
+        return np.sum(np.array(x[i]) * np.array(x[j]))
 
 getMatrixes()
-invMatrA = np.linalg.pinv(matrA)
-coefs = np.dot(invMatrA,matrB)
+coefs = np.dot(np.linalg.pinv(matrA),matrB) #b
 
-predY = np.dot(np.concatenate((np.ones((1,dataLen)),np.array(x))).T,coefs)
+nam = ['Ленинский','Октябрьский','Правобережный',
+       'кирпич','монолит-кирпич','панель','дерево',
+       'количество этажей','1й этаж','последний этаж',
+       'количество комнат','площадь','балкон']
+st = 'Стоимость = '+'%0.2f'%coefs[0]
+for i in range(11,varLen):
+    if coefs[i] < 0:
+        st += ' - '+'%0.2f'%abs(coefs[i])+' * '+nam[i-1]
+    else:
+        st += ' + '+'%0.2f'%coefs[i]+' * '+nam[i-1]
+for i in range(1,11):
+    if i==1 or i==4 or i==8:
+        st+='\n'
+    if coefs[i] < 0:
+        st += ' - '+'%0.2f'%abs(coefs[i])+' * '+nam[i-1]
+    else:
+        st += ' + '+'%0.2f'%coefs[i]+' * '+nam[i-1]
+print(st)
+
+
+
+testLen = len(yt)
+predY = np.dot(np.concatenate((np.ones((1,testLen)),np.array(xt))).T,coefs)
 sse = 0
 sst = 0
-ym = np.mean(y)
-for i in range(dataLen):
-    sse += pow(predY[i] - y[i], 2)
-    sst += pow(y[i] - ym, 2)
+ym = np.mean(yt)
+for i in range(testLen):
+    sse += pow(predY[i] - yt[i], 2)
+    sst += pow(yt[i] - ym, 2)
 r2 = 1 - sse/sst
-r2adj = 1 - (1 - r2) * (dataLen - 1) / (dataLen - varLen - 1)
-print('R2 = '+'%0.2f' %r2)
-print('R2adj = '+'%0.2f' %r2adj)
+r2adj = 1 - (1 - r2) * (testLen - 1) / (testLen - varLen - 1)
+print('R2 = '+'%0.9f' %r2)
+print('R2adj = '+'%0.9f' %r2adj)
 
 np.savetxt('/home/rjena/ApartmentPrices/LR/coefsLR.txt', coefs, fmt='%.15f')
